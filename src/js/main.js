7a7e4d9dd942b832b6d6cc458cc199c2bbbca1b5
vitalii_denysiuk <dendoc97@gmail.com>
2017-05-04 17:00:58 +0300

    add header

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,28 @@
 let template = require('./template');
 require("../css/main.scss");
-template.sayHi();
+(function () {
+var header = document.getElementsByTagName('header')[0];
+var iconMenu = document.getElementById('icon-menu');
+var menuList = document.getElementById('menu-list');
+var menuLink = document.querySelectorAll('.menu-link');
+var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
 
+if (isSafari) {
+    for (var i = 0; i < menuLink.length; i++) {
+      if (menuLink[i].dataset.scrollTo == '#portfolio') menuLink[i].parentElement.style.display = 'none';
+    }
+}
+
+menuList.style.top = header.offsetHeight + 'px';
+iconMenu.addEventListener('click', function () {
+  menuList.classList.toggle('show-menu');
+});
+
+var scrolled;
+document.addEventListener('scroll', function() {
+  scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  (scrolled > 100) ? header.classList.add('grey') : header.classList.remove('grey');
+});
+
+})();
 module.exports = template;
--- src/js/template.js
+++ src/js/template.js
@@ -1,5 +1,3 @@
 module.exports = {
-  sayHi: function() {
-    console.log('Hello world');
-  }
+
 }
