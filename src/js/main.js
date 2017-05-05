86e32ed3f509c91529ad1d038971678d909fe65f
vitalii_denysiuk <dendoc97@gmail.com>
2017-05-05 17:10:13 +0300

    add section 'programs'(done)

--- src/js/main.js
+++ src/js/main.js
@@ -25,4 +25,26 @@ document.addEventListener('scroll', function() {
 });
 
 })();
+
+var program = document.getElementsByClassName("program");
+var tab = document.getElementsByClassName("tab");
+
+[].forEach.call(program, function(item) {
+    item.addEventListener('click', function () {
+        for (var i = 0; i < tab.length; i++) {
+            if (this == program[i]) {
+                tab[i].classList.add("show");
+                this.classList.add("active-tab");
+            } else {
+              program[i].classList.remove("active-tab");
+              tab[i].classList.remove("show");
+            }
+        }
+    });
+});
+
+var width = window.innerWidth;
+
+
+
 module.exports = template;
